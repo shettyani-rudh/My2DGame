38dc855afe87093623daf31c67eaea1b0306ba81
Anirudh Shetty <anirudhshetty099@gmail.com>
2025-07-08 12:52:25 +0530

    Initial commit: Java 2D game with IntelliJ project setup

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+import javax.swing.*;
+import java.awt.*;
+    public class Main {
+        public static void main(String[] args) {
+            JFrame window = new JFrame();
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setResizable(false);
+            window.setTitle("2d Adventure");
+            window.setLocationRelativeTo(null);
+            window.setVisible(true);
+        }
+    }
+
