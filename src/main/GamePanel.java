ca41b1b1d6c9ab277f3515110dd1470f8fc7b64e
Anirudh Shetty <anirudhshetty099@gmail.com>
2025-07-23 21:06:44 +0530

    folder structure

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,44 +1,46 @@
 package main;
-
 import javax.swing.*;
 import java.awt.*;
 
-public  class GamePanel extends JPanel implements  Runnable{
+public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16;
     final int scale = 3;
-    final int tileSize = originalTileSize * scale;
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
+    final int tileSize = originalTileSize * scale; //width and height of tile
+    final int maxScreenCol = 16; //number of col tile 
+    final int maxScreenRow = 12;//number of row tile
     final int screenWidth = maxScreenCol * tileSize;
     final int screenheight = maxScreenRow * tileSize;
-    Thread  gameThread;
+    Thread gameThread;
 
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenheight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
 
-public   GamePanel(){
-    this.setPreferredSize(new Dimension(screenWidth,screenheight));
-    this.setBackground(Color.black);
-    this.setDoubleBuffered(true);
-}
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
 
-public  void startGameThread(){
-    gameThread = new Thread(this);
-    gameThread.start();
-}
     @Override
     public void run() {
         while (gameThread != null) {
-            //update character postion
-            //draw the screan with updated postion
+            update();
+            repaint();
 
         }
     }
-public void update(){
 
+    public void update() {
 
     }
-public void paintComponent(Graphics g){
 
-
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose();
     }
 }
-
